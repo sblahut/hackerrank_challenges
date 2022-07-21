4c4150fa28a5e688aed0662eb68fb327f9f86331
Steven Blahut <steven@commandc.com>
2022-07-20 21:58:13 -0400

    added example for level 2

---
+++ levelTwoExample.py
@@ -0,0 +1,6 @@
+#####   For example, given the list l as [“1.1.2”, “1.0”, “1.3.3”, “1.0.12”, “1.0.2”], 
+#####   the function solution(l) would return the list [“1.0”, “1.0.2”, “1.0.12”, “1.1.2”, “1.3.3”]. 
+#####   If two or more versions are equivalent but one version contains more numbers than the others, 
+#####   then these versions must be sorted ascending based on
+#####   how many numbers they have, e.g [“1”, “1.0”, “1.0.0”]. The number of elements in the 
+#####   list l will be at least 1 and will not exceed 100.
